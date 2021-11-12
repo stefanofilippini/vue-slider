c758bd4c94612e3966edec7a37118387656f68e2
stefanofilippini <filippini.stefano96@gmail.com>
2021-11-12 18:18:14 +0100

    done exercise

--- js/main.js
+++ js/main.js
@@ -1,33 +1,6 @@
 /*
  * Vue Slider
  */
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title:
-            'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    },
-]
 
 var app = new Vue({
     el: '#app',
@@ -55,9 +28,33 @@ var app = new Vue({
             },
             {
                 image: 'img/05.jpg',
-                title:
-                    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
             },
-        ]
+        ],
+
+        activePosition: 0,
+
+    },
+    methods : {
+        setSlide (index) {
+            this.activePosition = index
+        },
+
+        next () {
+            this.activePosition++;
+
+            if (this.activePosition === this.slides.length) {
+                this.activePosition = 0;
+            }
+        },
+
+        prev () {
+            this.activePosition--;
+
+            if (this.activePosition < 0) {
+                this.activePosition = this.slides.length - 1;
+            }
+        }
     }
   })
